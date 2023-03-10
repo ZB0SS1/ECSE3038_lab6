9d282241d2b4341783171b6d498fbfed538b2175
ZboSS <gallimoreraheim@gmail.com>
2023-03-10 16:47:38 -0500

    updated logic

--- api/app.py
+++ api/app.py
@@ -4,7 +4,9 @@ import motor.motor_asyncio
 import pydantic
 from bson import ObjectId
 from fastapi.middleware.cors import CORSMiddleware
+from geopy.geocoders import Nominatim
 import datetime
+import requests
 
 
 app = FastAPI()
@@ -28,13 +30,29 @@ states = db["state"]
 pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
 
 
-sunset_time = str(datetime.datetime.now()) 
+geolocator = Nominatim(user_agent="MyApp")
+
+location = geolocator.geocode("Hyderabad")
+
+user_latitude =  location.latitude
+user_longitude = location.longitude
+
+sunset_api_endpoint = f'https://api.sunrise-sunset.org/json?lat={user_latitude}&lng={user_longitude}'
+
+sunset_api_response = requests.get(sunset_api_endpoint)
+sunset_api_data = sunset_api_response.json()
+
+current_date = datetime.date.today()
+sunset_time = datetime.datetime.strptime(sunset_api_data['results']['sunset'], '%I:%M:%S %p').time()
+
+now_time = str(datetime.datetime.now())
 
 @app.put("/api/state")
 async def toggle(request: Request): 
   state = await request.json()
 
   lights_obj = await states.find_one({"tobe":"updated"})
+  lights_obj["sunset"] = sunset_time
   if lights_obj:
     await states.update_one({"tobe":"updated"}, {"$set": state})
 
@@ -50,6 +68,7 @@ async def toggle(request: Request):
 @app.get("/api/state")
 async def get_state():
   state = await states.find_one({"tobe": "updated"})
+  
   if state == None:
-    return {"temperature": 21.0, "sunset": sunset_time}
+    return {"temperature": False, "sunset": False}
   return state
