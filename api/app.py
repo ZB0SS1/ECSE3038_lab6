94be4056018526eb48e48fd8e09425443873cecb
ZboSS <gallimoreraheim@gmail.com>
2023-03-10 16:03:07 -0500

    testing something

--- api/app.py
+++ api/app.py
@@ -4,9 +4,7 @@ import motor.motor_asyncio
 import pydantic
 from bson import ObjectId
 from fastapi.middleware.cors import CORSMiddleware
-from geopy.geocoders import Nominatim
 import datetime
-import requests
 
 
 app = FastAPI()
@@ -30,21 +28,7 @@ states = db["state"]
 pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
 
 
-geolocator = Nominatim(user_agent="MyApp")
-
-location = geolocator.geocode("Hyderabad")
-
-user_latitude =  location.latitude
-user_longitude = location.longitude
-
-sunset_api_endpoint = f'https://api.sunrise-sunset.org/json?lat={user_latitude}&lng={user_longitude}'
-
-sunset_api_response = requests.get(sunset_api_endpoint)
-sunset_api_data = sunset_api_response.json()
-
-current_date = datetime.date.today()
-sunset_time = datetime.datetime.strptime(sunset_api_data['results']['sunset'], '%I:%M:%S %p').time()
-
+sunset_time = str(datetime.datetime.now()) 
 
 @app.put("/api/state")
 async def toggle(request: Request): 
